d7bd1f28f32eddcb4b4dff2bbe3b5879ab2d4549
Guno Kim <guno0302@gmail.com>
2020-04-07 21:33:42 +0900

    umjunsik

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
+import Sorry from './Sorry';
 import './App.css'
 
-export default function App() {
-  return(
-    <div>
-      <div className="HI">
-        <h1>왜 왔어</h1>
-        <span>다시 가</span>
+export default class App extends React.Component {
+  state = {
+    clicked: false
+  }
+
+  ftselec = () => {
+    if (this.state.clicked === true) {
+      this.setState({
+        clicked: false
+      })
+    } else {
+      this.setState({
+        clicked: true
+      })
+    }
+  }
+
+  render() {
+    const { clicked } = this.state
+    const ftselec = this.ftselec
+    return (
+      <div className="welcome">
+        <h1 className="main__text">Six Nerds WebPage</h1>
+        <input type="button" className="btn" value="이게 뭐죠?" onClick={ftselec} />
+        { clicked ? <Sorry /> : " "}
       </div>
-      <div className="Bye">
-        <h1>왜 갔어</h1>
-        <span>다시 와</span>
-      </div>
-      <div className="goTo">
-        <h1>맨위로 올라가</h1>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 //ctrl+k => ctrl+f
