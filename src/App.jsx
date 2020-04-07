b22e737146e54af54f7b1e64242ec924716a7b55
Guno Kim <guno0302@gmail.com>
2020-04-07 22:52:17 +0900

    today final

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,21 @@ export default class App extends React.Component {
     }
   }
 
+  redirect = () => {
+    console.log('코나미 커맨드 활성화')
+  }
+
+  appKeyPress = (e) => {
+    if (e.key === 'upArrow')
+      this.redirect()
+  }
+
   render() {
     const { clicked } = this.state
-    const ftselec = this.ftselec
+    const { ftselec, appKeyPress } = this
     return (
       <div className="center">
-        <div className="welcome">
+        <div className="welcome" onKeyPress={appKeyPress}>
           <h1 className="main__text">Six Nerds WebPage</h1>
           <input type="button" id="btn1" value="이게 뭐죠?" onClick={ftselec} />
           {clicked ? <Sorry /> : " "}
